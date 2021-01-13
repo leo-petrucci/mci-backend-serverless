1308ffb19c88feb824fb4720454d8830bfcc24b5
Leonardo Petrucci <leonardo@creativiii.com>
2021-01-13 20:11:56 +0000

    ⚗ Will this deploy?

--- api/_lib/schema.ts
+++ api/_lib/schema.ts
@@ -66,7 +66,23 @@ const Query = objectType({
 
 const Mutation = objectType({
   name: 'Mutation',
-  definition(t) {},
+  definition(t) {
+    t.field('updateTitle', {
+      type: 'Server',
+      args: {
+        id: nonNull(intArg()),
+        title: nonNull(stringArg()),
+      },
+      resolve: async (_, { title, id }, ctx) => {
+        return ctx.prisma.server.update({
+          where: { id: id },
+          data: {
+            title,
+          },
+        })
+      },
+    })
+  },
 })
 
 const generateArtifacts = Boolean(process.env.GENERATE_ARTIFACTS)
