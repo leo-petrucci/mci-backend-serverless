8fcdc5e0bc03e43527165a1902bb4bcd3658d96f
Leonardo Petrucci <leonardo@creativiii.com>
2021-01-13 23:01:59 +0000

    ⚗ This maybe might work

--- api/_lib/types.ts
+++ api/_lib/types.ts
@@ -1,4 +1,77 @@
-import { intArg, makeSchema, nonNull, objectType, stringArg } from 'nexus'
+import { objectType } from 'nexus'
+
+export const User = objectType({
+  name: 'User',
+  definition(t) {
+    t.model.id()
+    t.model.username()
+    t.model.email()
+    t.model.role()
+    t.model.banned()
+    t.model.photoUrl()
+    t.model.Votes()
+    t.model.Servers()
+  },
+})
+
+export const Tag = objectType({
+  name: 'Tag',
+  definition(t) {
+    t.model.id()
+    t.model.tagName()
+    t.model.Servers()
+    t.field('popularity', { type: 'Int' })
+  },
+})
+
+export const UserPayload = objectType({
+  name: 'UserPayload',
+  definition(t) {
+    t.field('user', { type: 'User' })
+  },
+})
+
+export const Version = objectType({
+  name: 'Version',
+  definition(t) {
+    t.model.id()
+    t.model.versionName()
+    t.model.Servers()
+  },
+})
+
+export const Vote = objectType({
+  name: 'Vote',
+  definition(t) {
+    t.model.id()
+    t.model.createdAt()
+    t.model.author()
+    t.model.server()
+  },
+})
+
+export const VoteCast = objectType({
+  name: 'VoteCast',
+  definition(t) {
+    t.field('outcome', { type: 'String' })
+  },
+})
+
+export const ServerPayload = objectType({
+  name: 'ServerPayload',
+  definition(t) {
+    t.field('id', { type: 'Int' })
+    t.field('published', { type: 'Boolean' })
+    t.field('createdAt', { type: 'String' })
+    t.field('title', { type: 'String' })
+    t.field('content', { type: 'String' })
+    t.field('author', { type: 'Int' })
+    t.field('tags', { type: 'Tag' })
+    t.field('version', { type: 'Version' })
+    t.field('slots', { type: 'Int' })
+    t.field('cover', { type: 'String' })
+  },
+})
 
 export const Server = objectType({
   name: 'Server',
@@ -10,14 +83,20 @@ export const Server = objectType({
     t.model.title()
     t.model.ip()
     t.model.content()
+    t.model.author()
+    t.model.tags()
+    t.model.version()
     t.model.slots()
     t.model.cover()
+    t.model.votes()
+    t.field('voteCount', { type: 'Int' })
+    t.field('canVote', { type: 'Boolean' })
   },
 })
 
-export const VoteCast = objectType({
-  name: 'VoteCast',
+export const AuthPayload = objectType({
+  name: 'AuthPayload',
   definition(t) {
-    t.field('outcome', { type: 'String' })
+    t.field('user', { type: 'User' })
   },
 })
