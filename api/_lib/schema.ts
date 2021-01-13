f1f79c6d94251c5af633cbfe401fb5ac63610393
Leonardo Petrucci <leonardo@creativiii.com>
2021-01-13 19:42:03 +0000

    ⚗ Added Server type

--- api/_lib/schema.ts
+++ api/_lib/schema.ts
@@ -37,6 +37,21 @@ const Profile = objectType({
   },
 })
 
+const Server = objectType({
+  name: 'Server',
+  definition(t) {
+    t.model.id()
+    t.model.published()
+    t.model.lastUpdated()
+    t.model.createdAt()
+    t.model.title()
+    t.model.ip()
+    t.model.content()
+    t.model.slots()
+    t.model.cover()
+  },
+})
+
 const Query = objectType({
   name: 'Query',
   definition(t) {
