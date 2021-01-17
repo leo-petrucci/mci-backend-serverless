6948d9f844558e777f5a855af6e6d01c3b9c3fc1
Leonardo Petrucci <leonardo@creativiii.com>
2021-01-17 21:58:01 +0000

    ⏪ Undo

--- api/_lib/check.ts
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-console.log(process.env.DATABASE_URL)
-console.log(process.env.APP_SECRET)
-
-if (process.env.DATABASE_URL === undefined) {
-  throw new Error('No database connection string was found')
-}
