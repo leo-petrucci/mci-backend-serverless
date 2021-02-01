f0eab73f47b8bb8ec4a3812c0beece3a604afc82
Leonardo Petrucci <leonardo@creativiii.com>
2021-02-01 18:14:54 +0000

    ⚗ Testing cors

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,13 @@ import { ApolloServer } from 'apollo-server-micro'
 import { schema } from './_lib/schema'
 import { createContext } from './_lib/context'
 import microCors from 'micro-cors'
+import { IncomingMessage, ServerResponse } from 'http'
 
-export const ALLOWED_ORIGIN: string = process.env.ALLOWED_ORIGIN!
+// export const ALLOWED_ORIGIN: string[] = process.env.ALLOWED_ORIGIN!.split(' ')
+export const ALLOWED_ORIGIN: string[] = [
+  'https://cs1v0.csb.app/',
+  'https://78wd5.csb.app/',
+]
 
 // const cors = microCors({
 //   allowCredentials: true,
@@ -21,9 +26,12 @@ const handler = server.createHandler({
   path: '/api',
 })
 
-const cors = (req, res) => {}
-
 // This is important or cors will fail at preflight
-export default cors((req, res) =>
-  req.method === 'OPTIONS' ? res.end() : handler(req, res),
-)
+export default (req: IncomingMessage, res: ServerResponse) => {
+  const origin = req.headers.origin!
+  if (ALLOWED_ORIGIN.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  return req.method === 'OPTIONS' ? res.end() : handler(req, res)
+}
