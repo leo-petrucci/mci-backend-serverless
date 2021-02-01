c9082297a80a68d426d7b3257efde9ef17caf443
Leonardo Petrucci <leonardo@creativiii.com>
2021-02-01 18:48:23 +0000

    🔧 This should allow multiple origins

--- api/index.ts
+++ api/index.ts
@@ -4,16 +4,7 @@ import { createContext } from './_lib/context'
 import microCors from 'micro-cors'
 import { IncomingMessage, ServerResponse } from 'http'
 
-// export const ALLOWED_ORIGIN: string[] = process.env.ALLOWED_ORIGIN!.split(' ')
-export const ALLOWED_ORIGIN: string[] = [
-  'https://cs1v0.csb.app/',
-  'https://78wd5.csb.app/',
-]
-
-// const cors = microCors({
-//   allowCredentials: true,
-//   origin: '*.csb.app',
-// })
+export const ALLOWED_ORIGIN: string[] = process.env.ALLOWED_ORIGIN!.split(' ')
 
 const server = new ApolloServer({
   schema,
@@ -28,6 +19,10 @@ const handler = server.createHandler({
 
 // This is important or cors will fail at preflight
 export default (req: IncomingMessage, res: ServerResponse) => {
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
   const origin = req.headers.origin!
   if (ALLOWED_ORIGIN.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin)
